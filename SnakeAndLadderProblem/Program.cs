27a67e2113d8e039d9f8c0a70bc5e21e987b937f
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-08 16:18:09 +0530

    [somu]Add:Single Player Start Position Zero.

---
+++ SnakeAndLadderProblem/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SnakeAndLadderProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Play play = new Play();
+            play.GamePlay();
+        }
+    }
+}
