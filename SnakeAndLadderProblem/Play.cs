7db2fa91b0121e4c9976ebe9902fa6767a2a7fb5
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-08 17:29:43 +0530

    [somu]Add:Player Checks for Option Noplay, Ladder and Snake.

--- SnakeAndLadderProblem/Play.cs
+++ SnakeAndLadderProblem/Play.cs
@@ -10,10 +10,26 @@ namespace SnakeAndLadderProblem
     {
         public void GamePlay()
         {
+           const int No_Play = 0, Ladder = 1, Snake = 2;
             int playerPosition = 0;
             Random random = new Random();
             int die=random.Next(1,7);
-            Console.WriteLine("Player Position: {0}",die);
+            //Console.WriteLine("Player Position: {0}",die);
+            int option=random.Next(0,3);
+            switch(option)
+            {
+                case No_Play:
+                    Console.WriteLine("Player position = Same Position");
+                    break;
+                case Ladder:
+                    playerPosition += die;
+                    Console.WriteLine("Player Position = " + playerPosition);
+                    break;
+                case Snake:
+                    playerPosition -= die;
+                    Console.WriteLine("Player Position = " + playerPosition);
+                    break;
+            }
         }
     }
 }
