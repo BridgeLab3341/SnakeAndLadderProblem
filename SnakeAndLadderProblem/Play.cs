50e79b87ef8028f2714ae67b2a0fc488130e7443
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-11 20:20:39 +0530

    [somu]Add:Playing Game With 2 Players and Reporting Which Player Won The Game.

--- SnakeAndLadderProblem/Play.cs
+++ SnakeAndLadderProblem/Play.cs
@@ -9,49 +9,85 @@ namespace SnakeAndLadderProblem
     public class Play
     {
         const int No_Play = 0, Ladder = 1, Snake = 2, Winning_Position = 100;
-        int playerPosition = 0;
+        int player1Position = 0;
+        int player2Position = 0;
+        int currentPlayer = 1;
         int count = 0;
         Random random = new Random();
+
         public int RollingDie()
         {
             int dieNum = random.Next(1, 7);
             count++;
             return dieNum;
         }
+
         public void GamePlay()
         {
-            
-            while (playerPosition<Winning_Position)
+            while (player1Position < Winning_Position && player2Position < Winning_Position)
             {
                 int option = random.Next(0, 3);
+                int roll = RollingDie();
+
                 switch (option)
                 {
                     case No_Play:
-                        Console.WriteLine("Player position = Same Position");
+                        Console.WriteLine($"Player {currentPlayer} position = Same Position");
                         break;
                     case Ladder:
-                        playerPosition += RollingDie();
-                        Console.WriteLine("Player Position = " + playerPosition);
-                        break;
-                    case Snake:
-                        int roll = RollingDie();
-                        if(playerPosition - roll >0)
+                        Console.WriteLine($"Player {currentPlayer} got a ladder and rolls again");
+                        if (currentPlayer == 1)
                         {
-                            playerPosition -= roll;
+                            player1Position += roll;
+                            Console.WriteLine($"Player 1 Position = {player1Position}");
                         }
                         else
                         {
-                            playerPosition = 0;
+                            player2Position += roll;
+                            Console.WriteLine($"Player 2 Position = {player2Position}");
                         }
-                        Console.WriteLine("Player Position = " + playerPosition);
+                        break;
+                    case Snake:
+                        if (currentPlayer == 1)
+                        {
+                            if (player1Position - roll > 0)
+                                player1Position -= roll;
+                            else
+                                player1Position = 0;
+                            Console.WriteLine($"Player 1 Position = {player1Position}");
+                        }
+                        else
+                        {
+                            if (player2Position - roll > 0)
+                                player2Position -= roll;
+                            else
+                                player2Position = 0;
+                            Console.WriteLine($"Player 2 Position = {player2Position}");
+                        }
                         break;
                 }
-                if(playerPosition == Winning_Position)
+
+                if (currentPlayer == 1)
                 {
-                    Console.WriteLine("Player Winning Position ----> " + playerPosition);
-                    Console.WriteLine("Die Count = "+ count);
+                    if (player1Position == Winning_Position)
+                    {
+                        Console.WriteLine("Player 1 won the game");
+                        Console.WriteLine($"Die Count = {count}");
+                        break;
+                    }
+                    currentPlayer = 2;
                 }
-            }           
+                else
+                {
+                    if (player2Position == Winning_Position)
+                    {
+                        Console.WriteLine("Player 2 won the game!");
+                        Console.WriteLine($"Die Count = {count}");
+                        break;
+                    }
+                    currentPlayer = 1;
+                }
+            }
         }
     }
 }
